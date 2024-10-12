78f7c7ff3ac9092f7069f579eab8e4d4f2cd6af6
Amir_Ahmed <amirmadian91@gmail.com>
2024-10-12 16:05:36 +0300

    create order info item

---
+++ lib/features/presentation/views/widgets/section1/order_info_item.dart
@@ -0,0 +1,28 @@
+import 'package:checkout_payment/core/utils/styles.dart';
+import 'package:checkout_payment/features/data/models/order_info_model.dart';
+import 'package:flutter/material.dart';
+
+class OrderInfoItem extends StatelessWidget {
+  const OrderInfoItem({
+    super.key, required this.orderInfoModel,
+  });
+   final OrderInfoModel  orderInfoModel;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+      children: [
+        Text(
+          orderInfoModel.item,
+          style: Styles.style18,
+        ),
+        const Spacer(),
+        Text(
+         orderInfoModel.value,
+          style: Styles.style18,
+        ),
+      ],
+    );
+  }
+}
