ccd588a0abd02f81b26e7e2b9398c8cde814db7c
Amir_Ahmed <amirmadian91@gmail.com>
2024-10-12 18:20:58 +0300

    edit app bar

--- lib/core/utils/functions/build_app_bar.dart
+++ lib/core/utils/functions/build_app_bar.dart
@@ -3,7 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 AppBar buildAppBar({required String? title}) => AppBar(
-      leading: Center(child: SvgPicture.asset('assets/images/back_arrow.svg',)),
+      leading: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: Center(
+            child: SvgPicture.asset(
+          'assets/images/back_arrow.svg',
+        )),
+      ),
       title: Text(
         '$title',
         style: Styles.style25,
