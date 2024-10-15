1da7881349c4854b4d66c64402f14c2a5e08df4b
Amir_Ahmed <amirmadian91@gmail.com>
2024-10-15 19:44:10 +0300

    add puplic key to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
-import 'package:checkout_payment/features/presentation/views/my_cart_view.dart';
+import 'package:checkout_payment/core/utils/api_keys.dart';
+import 'package:checkout_payment/features/checkout/presentation/views/my_cart_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey =ApiKeys.stripePublishablekey;
   runApp(const CheckOutPayment());
 }
 
