bda2c806cd2da1d202d38eadbac70223d050754b
Amir_Ahmed <amirmadian91@gmail.com>
2024-10-15 16:48:56 +0300

    create api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,55 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class ApiService {
+  final Dio _dio;
+  ApiService(this._dio);
+  Future<Response> getRequest(
+      {required String baseUrl,
+      required String endPoint,
+      @required String? token,
+      @required String? contentType,
+      Map<String, dynamic>? headersInput}) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    Response response = await _dio.get(
+      '$baseUrl$endPoint',
+      options: Options(
+        contentType: contentType,
+        headers: headersInput ?? headers,
+        sendTimeout: const Duration(seconds: 7),
+        receiveTimeout: const Duration(seconds: 7),
+      ),
+    );
+    return response;
+  }
+
+  Future<Response> postRequest({
+    required String baseUrl,
+    required String endPoint,
+    @required String? token,
+    @required String? contentType,
+    required Map<String, dynamic> body,
+    Map<String, dynamic>? headersInput,
+  }) async {
+    Map<String, String> headers = {};
+
+    if (token != null) {
+      headers.addAll({'Authorization': 'Bearer $token'});
+    }
+    Response response = await _dio.post(
+      '$baseUrl$endPoint',
+      data: body,
+      options: Options(
+        contentType: contentType,
+        headers: headersInput ?? headers,
+        sendTimeout: const Duration(seconds: 7),
+        receiveTimeout: const Duration(seconds: 7),
+      ),
+    );
+    return response;
+  }
+}
